14dbf717797d35bb2438f6f264d73662c88a545c
Dan Simons <dansimons00@gmail.com>
2024-11-24 19:53:41 -0700

    change quarto clear keymap + python formatter

--- ftplugin/quarto.lua
+++ ftplugin/quarto.lua
@@ -11,6 +11,6 @@ end
 -- qmd shortcuts
 map('n', '<leader>py', ":lua require('nvim-autopairs').disable()<CR>o```{python}<CR>```<esc>:lua require('nvim-autopairs').enable()<CR>O")
 map('n', '<leader>ci', ':vsplit term://ipython<CR> <C-h>')
-map('n', '<C-c>', '<C-l>clear<CR><C-h>')
+map('n', '<leader>cl', '<C-l>clear<CR><C-h>')
 map('n', '<leader>p', ':call slime#send_cell()<CR>')
 map('n', '<leader>P', ':call slime#send_cell()<CR>/```{python}<CR><esc>')
